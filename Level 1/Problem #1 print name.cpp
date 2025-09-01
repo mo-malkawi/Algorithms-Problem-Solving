11e319d809c4d6c23f87ed71c8f6dc6f7b8a9617
Mohammad Malkawi <mo.malkawi7@gmail.com>
2025-09-01 11:02:24 +0300

    Add files via upload

---
+++ Level 1/Problem #1 print name.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintName(string name)
+{
+    cout << "Your name is : " << name << endl;
+}
+
+int main()
+{
+    PrintName("Mohammad");
+
+    return 0;
+
+}
+
+
